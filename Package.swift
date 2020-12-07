ee69ff7b3eed4d311d9b642ac4c8565ad043a0fd
neutralradiance <70107970+neutralradiance@users.noreply.github.com>
2020-12-07 09:59:58 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/neutralradiance/swift-light.git", .branch("master")),
+        .package(name: "Light", url: "https://github.com/neutralradiance/swift-light.git", .branch("master")),
         .package(name: "Yams", url: "https://github.com/jpsim/yams.git", .branch("master"))
     ],
     targets: [
