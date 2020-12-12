b50e5522d48b8763e78b036fe20e1a7b29a3a1e6
neutralradiance <70107970+neutralradiance@users.noreply.github.com>
2020-12-12 00:08:08 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "Light", url: "https://github.com/neutralradiance/swift-light.git", .branch("master")),
-        .package(name: "Yams", url: "https://github.com/jpsim/yams.git", .branch("master"))
+        .package(name: "Yams", url: "https://github.com/jpsim/yams.git", from: "4.0.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
